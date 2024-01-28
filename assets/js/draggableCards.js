72124a8c15017815b2c3b02fea227b12fcb6750d
elindstr <elindstr@gmail.com>
2024-01-28 00:30:15 -0800

    cleanup

--- assets/js/draggableCards.js
+++ assets/js/draggableCards.js
@@ -3,7 +3,7 @@
 var drake = dragula([document.getElementById('secondary-cards'), document.getElementById('primary-cards')], {
     accepts: function (el, target, source, sibling) {
         if (target === document.getElementById('primary-cards')) {
-            return target.children.length < 2;
+            return target.children.length < 2
         }
         else {
             return true
