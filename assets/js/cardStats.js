8833f449701791d42e5391c8699e583ea6382744
elindstr <elindstr@gmail.com>
2024-02-06 19:03:39 -0800

    added promote to primary-cards button

--- assets/js/cardStats.js
+++ assets/js/cardStats.js
@@ -150,11 +150,162 @@ async function createFeaturePlayerCard(player_id, element) {
         .attr('class', 'slash-table')
     slashTableSection.append(slashTable)
 
-    
     $(element).empty() //XXX testing 
     $(element).append(playerNameSection, teamNameSection, slashTableSection)
     killDuplicates()
 }
+
+// This is stupid. But this is a fix.
+async function createFeaturePlayerCardFromScratch(player_id) {
+    console.log(player_id)
+
+    // get stats
+    const playerDetails = await getPlayerDetails(player_id)
+    const primary_position_txt = playerDetails.primary_position_txt
+    const team_id = playerDetails.team_id
+    const name_display_first_last = playerDetails.name_display_first_last
+
+    // load media (team_id, player_id)
+    teamLogo = await getTeamLogo(team_id)
+    const playerImage = await getPlayerImage(player_id, name_display_first_last)    // TODO: Debug
+
+    // seasonPositionStats depending on hitter or pitcher
+    let seasonPositionStats
+    if (primary_position_txt == 'P') {
+        seasonPositionStats2023 = await getSeasonPitcherStats(player_id, year = 2023)
+        seasonPositionStats2022 = await getSeasonPitcherStats(player_id, year = 2022)
+        careerPositionStats = await getCareerPitcherStats(player_id)
+    }
+    else {
+        seasonPositionStats2023 = await getSeasonHitterStats(player_id, year = 2023)
+        seasonPositionStats2022 = await getSeasonHitterStats(player_id, year = 2022)
+        careerPositionStats = await getCareerHitterStats(player_id)
+    }
+
+    // create slash line
+    if (playerDetails.primary_position_txt == 'P') {
+        slashTable = $('<table>');
+        slashRow = $('<tr>');
+        slashThYear = $('<th>Year</th>');
+        slashThERA = $('<th>ERA</th>');
+        slashThWHIP = $('<th>WHIP</th>');
+        slashThBBK = $('<th>BB:K</th>');
+        slashRow.append(slashThYear, slashThERA, slashThWHIP, slashThBBK);
+        slashTable.append(slashRow);
+
+        slashRow = $('<tr>')
+        eraCareer = careerPositionStats ? careerPositionStats.era : na
+        whipCareer = careerPositionStats ? careerPositionStats.whip : na
+        kbbCareer = careerPositionStats ? careerPositionStats.kbb : na
+        slashTdYear = $(`<td>Career</td>`)
+        slashTdERA = $(`<td>${eraCareer ? eraCareer : na}</td>`)
+        slashTdWHIP = $(`<td>${whipCareer ? whipCareer : na}</td>`)
+        slashTdKBB = $(`<td>${kbbCareer ? kbbCareer : na}</td>`)
+        slashRow.append(slashTdYear, slashTdERA, slashTdWHIP, slashTdKBB)
+        slashTable.append(slashRow)
+
+        if (seasonPositionStats2023) {
+            slashRow = $('<tr>')
+            era2023 = seasonPositionStats2023.era
+            whip2023 = seasonPositionStats2023.whip
+            kbb2023 = seasonPositionStats2023.kbb
+            slashTdYear = $(`<td>2023</td>`)
+            slashTdERA = $(`<td>${era2023 ? era2023 : na}</td>`)
+            slashTdWHIP = $(`<td>${whip2023 ? whip2023 : na}</td>`)
+            slashTdKBB = $(`<td>${kbb2023 ? kbb2023 : na}</td>`)
+            slashRow.append(slashTdYear, slashTdERA, slashTdWHIP, slashTdKBB)
+            slashTable.append(slashRow)
+        }
+
+        if (seasonPositionStats2022) {
+            slashRow = $('<tr>')
+            era2022 = seasonPositionStats2022.era
+            whip2022 = seasonPositionStats2022.whip
+            kbb2022 = seasonPositionStats2022.kbb
+            slashTdYear = $(`<td>2022</td>`)
+            slashTdERA = $(`<td>${era2022 ? era2022 : na}</td>`)
+            slashTdWHIP = $(`<td>${whip2022 ? whip2022 : na}</td>`)
+            slashTdKBB = $(`<td>${kbb2022 ? kbb2022 : na}</td>`)
+            slashRow.append(slashTdYear, slashTdERA, slashTdWHIP, slashTdKBB)
+            slashTable.append(slashRow)
+        }
+    }
+    else { // hitter
+        slashTable = $('<table>');
+        slashRow = $('<tr>');
+        slashThYear = $('<th>Year</th>');
+        slashThAvg = $('<th>Avg</th>');
+        slashThOBP = $('<th>OBP</th>');
+        slashThSLG = $('<th>SLG</th>');;
+        slashRow.append(slashThYear, slashThAvg, slashThOBP, slashThSLG);
+        slashTable.append(slashRow);
+
+        slashRow = $('<tr>')
+        eraCareer = careerPositionStats ? careerPositionStats.avg : na
+        whipCareer = careerPositionStats ? careerPositionStats.obp : na
+        ipCareer = careerPositionStats ? careerPositionStats.slg : na
+        slashTdYear = $(`<td>Career</td>`)
+        slashTdERA = $(`<td>${eraCareer ? eraCareer : na}</td>`)
+        slashTdWHIP = $(`<td>${whipCareer ? whipCareer : na}</td>`)
+        slashTdIP = $(`<td>${ipCareer ? ipCareer : na}</td>`)
+        slashRow.append(slashTdYear, slashTdERA, slashTdWHIP, slashTdIP)
+        slashTable.append(slashRow)
+
+        if (seasonPositionStats2023) {
+            slashRow = $('<tr>')
+            era2023 = seasonPositionStats2023.avg
+            whip2023 = seasonPositionStats2023.obp
+            ip2023 = seasonPositionStats2023.slg
+            slashTdYear = $(`<td>2023</td>`)
+            slashTdERA = $(`<td>${era2023 ? era2023 : na}</td>`)
+            slashTdWHIP = $(`<td>${whip2023 ? whip2023 : na}</td>`)
+            slashTdIP = $(`<td>${ip2023 ? ip2023 : na}</td>`)
+            slashRow.append(slashTdYear, slashTdERA, slashTdWHIP, slashTdIP)
+            slashTable.append(slashRow)
+        }
+
+        if (seasonPositionStats2022) {
+            slashRow = $('<tr>')
+            era2022 = seasonPositionStats2022.avg
+            whip2022 = seasonPositionStats2022.obp
+            ip2022 = seasonPositionStats2022.slg
+            slashTdYear = $(`<td>2022</td>`)
+            slashTdERA = $(`<td>${era2022 ? era2022 : na}</td>`)
+            slashTdWHIP = $(`<td>${whip2022 ? whip2022 : na}</td>`)
+            slashTdIP = $(`<td>${ip2022 ? ip2022 : na}</td>`)
+            slashRow.append(slashTdYear, slashTdERA, slashTdWHIP, slashTdIP)
+            slashTable.append(slashRow)
+        }
+    }
+
+    // construct card
+    let playerNameSection = $('<span>')
+    .attr('class', 'card-player-name');
+    
+    const deleteButton = $('<button>')
+    .attr('class', 'delete-button')
+    .html('&times;');
+    playerNameSection.append(playerImage, name_display_first_last, deleteButton);
+
+    let teamNameSection = $('<span>')
+    .attr('class', 'card-team-name');
+    teamNameSection.append(teamLogo);
+
+    let slashTableSection = $('<span>')
+    .attr('class', 'slash-table');
+    slashTableSection.append(slashTable);
+
+    let newCard = $('<div>')        
+        .attr('data-id', player_id)
+        .attr('class', 'card')
+         
+    newCard.append(playerNameSection, teamNameSection, slashTableSection);
+
+    $('#primary-cards').append(newCard);
+    checkforEmptyDropBoxes()
+    killDuplicates()
+} 
+
 
 $('#primary-cards').on('click', '.delete-button', function () {
     $(this).parent().parent().remove()
@@ -165,9 +316,30 @@ $('#secondary-cards').on('click', '.delete-button', function () {
     checkforEmptyDropBoxes()
 });
 
+$('#secondary-cards').on('click', '.promote-button', function () {
+    
+    // if two features, demote second card
+    if ($('#primary-cards').children().length === 2) {
+        const demotedPlayerId = $('#primary-cards').children().eq(1).attr('data-id');
+        //console.log(demotedPlayerId)
+        $('#primary-cards').children().eq(1).remove();
+        createPlayerCard(demotedPlayerId); // Assuming this function exists
+    }
+
+    const promotedPlayerId = $(this).parent().attr('data-id');
+    //console.log(promotedPlayerId)
+
+    // remove element from #secondary-cards
+    $(this).parent().remove()
+
+    // promote promotedPlayerId to #primary-cards
+    createFeaturePlayerCardFromScratch(promotedPlayerId)
+});
+
+
 // called by autoComplete.js when user selects a player from the search
 async function createPlayerCard(player_id) {
-    console.log("new player card", player_id)
+    //console.log("new player card", player_id)
 
     // get stats
     const playerDetails = await getPlayerDetails(player_id)
@@ -186,7 +358,12 @@ async function createPlayerCard(player_id) {
     const deleteButton = $('<button>')
         .attr('class', 'delete-button')
         .html('&times;')
-    newCard.append(playerImage, name_display_first_last, deleteButton)
+    
+    const promoteButton = $('<button>')
+        .attr('class', 'promote-button')
+        .html('&uarr;')
+
+    newCard.append(promoteButton, playerImage, name_display_first_last, deleteButton)
 
     // render to page if player not already there
     if (cardExists = $('#primary-cards').find(`#${player_id}`).length > 0) {
